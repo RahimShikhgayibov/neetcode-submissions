f31dd244f353bfd9aec8805170fac7252a19d755
Rahim <rehim1718@gmail.com>
2026-04-16 14:04:32 +0400

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        //hashmap solution
+        if(s.size()!=t.size()){
+            return false;
+        }
+
+        unordered_map<int,int> mp1;
+        unordered_map<int,int> mp2;
+
+        for(int i=0;i<s.size();i++){
+            mp1[s[i]]++;
+            mp2[t[i]]++;
+        }
+
+        if(mp1==mp2){
+            return true;
+        }
+        return false;
+    }
+};
